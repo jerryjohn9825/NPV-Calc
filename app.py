e150d097907ff71df4fb0b52cdb1032e15ec0ff1
jerryjohn9825 <148794094+jerryjohn9825@users.noreply.github.com>
2024-08-24 16:04:27 -0400

    Add os import to fix NameError

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
@@ -141,4 +142,4 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
